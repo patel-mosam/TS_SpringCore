b11218c1fe46d315df8881128526feab5194ab68
patel-mosam <mosampatel2904@.com>
2024-09-15 14:43:45 +0530

    third

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -6,21 +6,18 @@ package com;
  */
 public class App 
 {
-    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
-        System.out.println( "Welcome to the World!" );
-<<<<<<< HEAD
+
+        System.out.println( "Mosam" );
+
         System.out.println("Punit");
-=======
+
         //jhanvi
->>>>>>> f91f3dfa6a263564ac4c12314d47863284d25cca
-=======
+
         // hello world
         // kashish
         
 
->>>>>>> e2bd2b3a7e45706a653dc045736b86ad64355702
     }
 }
